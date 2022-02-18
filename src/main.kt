0e04705a64b46a8da64aa433a888b07721ba2585
rehemajaffar <rehemajumajaffar@gmail.com>
2022-02-18 21:31:57 +0300

    firstcommit

---
+++ src/main.kt
@@ -0,0 +1,29 @@
+fun main(){
+    name()
+    module()
+    sum()
+    interesting()
+
+}
+fun  name(){
+    var name ="rehema"
+    println("hello "+name)
+}
+fun module(){
+    var a =25
+    var b =3
+    var modulus=a%b
+    println(modulus)
+}
+fun sum (){
+    var a =45
+    var b =60
+    var c =99
+    var d =45
+    var sum =a+b+c+d
+    println(sum)
+}
+fun interesting(){
+    var interesting ="i like drawing"
+    println(interesting)
+}
